9b72e5eb2c8fcfa5bcf39fcd2c34d602e65df8ab
gujaru <choyacs@gmail.com>
2021-04-02 16:03:32 +1100

    End of section 7

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle(): void {
+    this.registerMode = !this.registerMode;
+    console.log(this.registerMode);
+  }
+
+
+  cancelRegisterMode(event: boolean): void {
+    this.registerMode = event;
+  }
+}
